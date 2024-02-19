45c0382cef69468fbb95a72d0d015f7285e158b2
shashikantsarade <109937090+shashikantsarade@users.noreply.github.com>
2024-02-19 19:26:35 +0530

    Update ocr_main.py

--- ocr_main.py
+++ ocr_main.py
@@ -7,7 +7,7 @@ from PIL import Image
 import pytesseract
 
 # Set Tesseract path (change it as per your system configuration)
-#pytesseract.pytesseract.tesseract_cmd = r'<path_to_tesseract_executable>'
+#pytesseract.pytesseract.tesseract_cmd = none
 
 def ocr_text(image):
     # Perform OCR on the image
